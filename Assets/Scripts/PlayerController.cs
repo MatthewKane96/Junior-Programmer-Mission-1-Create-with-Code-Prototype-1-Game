518c8346f71e2c0f7308b2b9c2a2dcf86ca013d6
MatthewKane96 <mkane38@qub.ac.uk>
2025-09-21 20:32:21 +0100

    Input set to both
    Issue with user input below that  causes break and doesn't allow vehicle to move. Resolved
    Solution : - Go to Edit Menu in Unity, got to project settings, go to the list on the side left click player, expand other setting, scroll down and find configuration and change active input handling to Both

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float turnspeed;
     //public float turnspeed = 45.0f;
-    //public float horizontalInput;
+    public float horizontalInput;
     //private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,15 +24,15 @@ public class PlayerController : MonoBehaviour
         // Solution : - Go to Edit Menu in Unity, got to project settings, go to the list on the side left click player, expand other setting, scroll down and find configuration and change active input handling to Both
 
         // This is where we get Player input
-        //horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         // forwardInput = Input.GetAxis("Vertical");
 
         // We Move the vehicle Vertically Forward and back
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Move the vehicle Horizontally
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
-        //transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
+        //Stage 2 transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
         // We Turn the vehicle based on Rotation via horizontal input 
         //transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
 
