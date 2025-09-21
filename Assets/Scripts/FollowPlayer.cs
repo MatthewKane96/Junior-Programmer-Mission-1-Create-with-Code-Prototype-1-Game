2ed63a640610a63900396792264181ec05cdcf56
MatthewKane96 <mkane38@qub.ac.uk>
2025-09-21 20:16:50 +0100

    Update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -15,7 +15,8 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         // Offset the camera behind the player by adding to the player's position  
-        transform.position = player.transform.position;
+        //transform.position = player.transform.position;
+        transform.position = player.transform.position + new Vector3(0, 4, -10);
         //transform.position = player.transform.position + offset;
     }
 }
