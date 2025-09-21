3af0120d9d1c1e4429eee2f45759b9f728596e66
MatthewKane96 <mkane38@qub.ac.uk>
2025-09-21 20:08:37 +0100

    Programming

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    //private Vector3 offset = new Vector3(0, 4, -10); // or change value to (0,5,-13) for better camera for van model
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Offset the camera behind the player by adding to the player's position  
+        transform.position = player.transform.position;
+        //transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Private Variables
+    public float speed = 20.0f;
+    //private float turnspeed = 45.0f;
+    //private float horizontalInput;
+    //private float forwardInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Issue with user input below that  causes break and doesn't allow vehicle to move. Resolved
+        // Solution : - Go to Edit Menu in Unity, got to project settings, go to the list on the side left click player, expand other setting, scroll down and find configuration and change active input handling to Both
+
+        // This is where we get Player input
+        //horizontalInput = Input.GetAxis("Horizontal");
+        // forwardInput = Input.GetAxis("Vertical");
+
+        // We Move the vehicle Vertically Forward and back
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We Turn the vehicle based on Rotation via horizontal input 
+        //transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
+        // Move the vehicle Horizontally
+        // transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
